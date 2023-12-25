c2cdb087d5d78961471d130ed98d1dae19caf2d3
Nils Nahooy <nils.nahooy@live.com>
2023-12-25 15:22:24 +0100

    follow along (47:00)

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,21 +1,58 @@
-"use client"
+"use client";
 
-import React from 'react'
-import styled from 'styled-components'
-import { useGlobalState } from '@/app/context/globalProvider'
+import { useGlobalState } from "@/app/context/globalProvider";
+import menu from "@/app/utils/menu";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import styled from "styled-components";
 
 function Sidebar() {
-	const { theme } = useGlobalState()
-	return (
-		<SidebarStyled theme={theme}>Sidebar</SidebarStyled>
-	)
+  const { theme } = useGlobalState();
+  const router = useRouter();
+  const pathName = usePathname();
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
+
+  return (
+    <SidebarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay" />
+        <div className="image">
+          <Image width={70} height={70} src="/avatar.jpeg" alt="profile" />
+        </div>
+        <h1>
+          <span>John</span>
+          <span>Doe</span>
+        </h1>
+        <ul className="nav-items">
+          {menu.map((item) => {
+            return (
+              <li
+                className={`nav-item ${pathName === item.link ? "active" : ""}`}
+                key={item.id}
+                onClick={() => {
+                  handleClick(item.link);
+                }}
+              >
+                {item.icon}
+                <Link href={item.link}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </SidebarStyled>
+  );
 }
 
 const SidebarStyled = styled.nav`
-	position: relative;
-	width: ${(props) => props.theme.sidebarWidth};
-	background-color: ${(props) => props.theme.colorBg2};
-	border-right: 2px solid ${(props) => props.theme.borderColor2};
-	border-radius: 1rem;
-`
-export default Sidebar
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
+export default Sidebar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import Sidebar from './Components/Sidebar/Sidebar'
-import GlobalStyleProvider from './providers/GlobalStyleProvider'
+import './globals.css'
 import ContextProvider from './providers/ContextProvider'
+import GlobalStyleProvider from './providers/GlobalStyleProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body className={inter.className}>
         <ContextProvider>
           <GlobalStyleProvider>

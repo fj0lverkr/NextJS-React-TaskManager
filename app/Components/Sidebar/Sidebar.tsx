fcad99366dcfa398f00e927c2a8b24bf449a9243
Nils Nahooy <nils.nahooy@live.com>
2023-12-25 17:19:54 +0100

    Follow along (56:18)

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -27,23 +27,23 @@ function Sidebar() {
           <span>John</span>
           <span>Doe</span>
         </h1>
-        <ul className="nav-items">
-          {menu.map((item) => {
-            return (
-              <li
-                className={`nav-item ${pathName === item.link ? "active" : ""}`}
-                key={item.id}
-                onClick={() => {
-                  handleClick(item.link);
-                }}
-              >
-                {item.icon}
-                <Link href={item.link}>{item.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
       </div>
+      <ul className="nav-items">
+        {menu.map((item) => {
+          return (
+            <li
+              className={`nav-item ${pathName === item.link ? "active" : ""}`}
+              key={item.id}
+              onClick={() => {
+                handleClick(item.link);
+              }}
+            >
+              {item.icon}
+              <Link href={item.link}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </SidebarStyled>
   );
 }
@@ -54,5 +54,75 @@ const SidebarStyled = styled.nav`
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: ${(props) => props.theme.colorGrey3};
+
+  .profile {
+    margin: 1.5rem;
+    padding: 1rem 0.8rem;
+    position: relative;
+    border-radius: 1rem;
+    cursor: pointer;
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+    display: flex;
+    align-items: center;
+    .profile-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(10px);
+      z-index: 0;
+      background: ${(props) => props.theme.colorBg3};
+      transition: all 0.55s linear;
+      border-radius: 1rem;
+      opacity: 0.2;
+      border: 2px solid ${(props) => props.theme.borderColor2};
+    }
+    h1 {
+      font-size: 1.2rem;
+      display: flex;
+      flex-direction: column;
+      line-height: 1.4rem;
+    }
+    .image,
+    h1 {
+      position: relative;
+      z-index: 1;
+    }
+    .image {
+      flex-shrink: 0;
+      display: inline-block;
+      overflow: hidden;
+      transition: all 0.5s ease;
+      border-radius: 100%;
+      width: 70px;
+      height: 70px;
+      img {
+        border-radius: 100%;
+        transition: all 0.5s ease;
+      }
+    }
+    > h1 {
+      margin-left: 0.8rem;
+      font-size: clamp(1.2rem, 4vw, 1.4rem);
+      line-height: 100%;
+    }
+    &:hover {
+      .profile-overlay {
+        opacity: 1;
+        border: 2px solid ${(props) => props.theme.borderColor2};
+      }
+      img {
+        transform: scale(1.1);
+      }
+    }
+  }
 `;
+
 export default Sidebar;

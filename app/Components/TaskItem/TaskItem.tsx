05b2c0c8770a8b22ddefc8c033f786684bd883fc
Nils Nahooy <nils.nahooy@live.com>
2023-12-28 15:41:36 +0100

    implement delete button

--- app/Components/TaskItem/TaskItem.tsx
+++ app/Components/TaskItem/TaskItem.tsx
@@ -22,7 +22,7 @@ function TaskItem({
   isImportant,
   id,
 }: Props) {
-  const { theme } = useGlobalState();
+  const { theme, deleteTask } = useGlobalState();
 
   return (
     <TaskItemStyled theme={theme}>
@@ -36,7 +36,9 @@ function TaskItem({
           <button className="incomplete">Incomplete</button>
         )}
         <button className="edit">{edit}</button>
-        <button className="delete">{trash}</button>
+        <button className="delete" onClick={() => deleteTask(id)}>
+          {trash}
+        </button>
       </div>
     </TaskItemStyled>
   );
